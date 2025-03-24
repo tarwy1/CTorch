9fa6fadff241d8decf59e2c888fb077459bae6b4
Charlie Headon <headoncharlie@gmail.com>
2025-03-24 14:38:05 +0000

    Switched to float based logic

--- compile.bat
+++
@@ -1,7 +0,0 @@
-gcc -c -g ct_network.c
-gcc -c -g ct_optimizer.c
-gcc -c -g ct_util.c
-gcc -c -g ct_layer.c
-gcc -c -g main.c
-
-gcc -o main.exe ct_network.o ct_optimizer.o ct_util.o ct_layer.o main.o
